1d77d58a64db901ef9a524039663a170aaff9ebd
Gaurav-Swami <gauravswami0404@gmail.com>
2024-09-09 23:44:15 +0530

    commit

--- app2/App.tsx
+++ app2/App.tsx
@@ -1,6 +1,8 @@
 import {View, Text, SafeAreaView, ScrollView} from 'react-native';
 import React from 'react';
 import FlatCards from './components/FlatCards';
+import ElevatedCards from './components/ElevatedCards';
+import FancyCard from './components/FancyCard';
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <ScrollView>
         
         <FlatCards />
+        <ElevatedCards/>
+        <FancyCard/>
       </ScrollView>
     </SafeAreaView>
   );
---
+++ app2/components/FancyCard.tsx
@@ -0,0 +1,63 @@
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function FancyCard() {
+  return (
+    <View>
+      <Text style={styles.headingText}>Trending Places</Text>
+      <View style={[styles.card, styles.cardElevated]}>
+        <Image
+          source={{
+            uri: 'https://d38b044pevnwc9.cloudfront.net/cutout-nuxt/enhancer/2.jpg',
+          }}
+          style={styles.cardImage}
+        />
+        <View style={styles.cardBody}>
+          <Text style={styles.cardTitle}>Eiffel Tower</Text>
+          <Text style={styles.cardLabel}>Paris, France</Text>
+          <Text style={styles.cardDescription}>
+            The Eiffel Tower is famous tourist location in the city of Paris,
+            built with pure aluminiam. It is one of the best location to travel
+            to.
+          </Text>
+          <Text style={styles.cardFooter}>12 mins away</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  headingText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    paddingHorizontal: 8,
+    marginBottom: 8,
+  },
+  card: {
+    width: 360,
+    height: 360,
+    borderRadius: 8,
+    marginVertical: 12,
+    marginHorizontal: 16,
+  },
+  cardElevated: {
+    backgroundColor: '#FFFFFF',
+    elevation: 3,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+  },
+  cardImage: {
+    height: 180,
+    marginBottom: 8,
+    borderTopLeftRadius: 8, 
+    borderTopRightRadius: 8
+  },
+  cardBody: {},
+  cardTitle: {},
+  cardLabel: {},
+  cardDescription: {},
+  cardFooter: {},
+});
